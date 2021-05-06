78fc55b41a31873635caa89e4e457cb27b51fc04
DESKTOP-2CTP7F6\xutia <853122726@qq.com>
2021-05-06 11:30:55 +0800

    u

--- src/main/java/club/koumakan/framework/base/apiconfig/service/impl/ApiConfigServiceImpl.java
+++ src/main/java/club/koumakan/framework/base/apiconfig/service/impl/ApiConfigServiceImpl.java
@@ -1,16 +1,16 @@
 package club.koumakan.framework.base.apiconfig.service.impl;
 
+import club.koumakan.framework.base.apiconfig.dao.ApiConfigDAO;
 import club.koumakan.framework.base.apiconfig.entity.ApiConfig;
 import club.koumakan.framework.base.apiconfig.service.ApiConfigService;
 import club.koumakan.framework.common.abstractapi.impl.FrameworkServiceApiImpl;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ApiConfigServiceImpl extends FrameworkServiceApiImpl<ApiConfig> implements ApiConfigService {
 
-    public ApiConfigServiceImpl(JpaRepository<ApiConfig, Long> dao) {
-        super(dao);
+    public ApiConfigServiceImpl(ApiConfigDAO apiConfigDAO) {
+        super(apiConfigDAO);
     }
 
     @Override
