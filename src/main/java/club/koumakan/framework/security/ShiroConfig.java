d58e63b7a5ddf0e132135f58c8f4fee2f5e09d03
DESKTOP-2CTP7F6\xutia <853122726@qq.com>
2021-07-26 18:00:07 +0800

    u

--- src/main/java/club/koumakan/framework/security/ShiroConfig.java
+++ src/main/java/club/koumakan/framework/security/ShiroConfig.java
@@ -2,6 +2,7 @@ package club.koumakan.framework.security;
 
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,7 +25,7 @@ public class ShiroConfig {
         // anon 任何人都能访问，如登录页面
         filterMap.put("/base/user/login", "anon");
         // authc 需要登录才能访问，如系统内的全体通知页面
-        filterMap.put("/base/user/findAll", "authc");
+//        filterMap.put("/base/user/findAll", "authc");
         // roles 需要相应的角色才能访问
 //        filterMap.put("/api/user/getUser", "roles[admin]");
         // 5. 让ShiroFilter按这个规则拦截
@@ -42,5 +43,12 @@ public class ShiroConfig {
         // SecurityManager既然管理认证等信息，那他就需要一个类来帮他查数据库吧。这就是Realm类
         securityManager.setRealm(shiroRealm);
         return securityManager;
+    }
+
+    @Bean
+    public DefaultAdvisorAutoProxyCreator getDefaultAdvisorAutoProxyCreator() {
+        DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator = new DefaultAdvisorAutoProxyCreator();
+        defaultAdvisorAutoProxyCreator.setUsePrefix(true);
+        return defaultAdvisorAutoProxyCreator;
     }
 }
